a085777efb21aa65d71e5cb452107e8ccae7fab4
Joe Cridge <joe.cridge@me.com>
2016-06-19 18:27:18 +0100

    Add scripts to convert between hex and modhex

---
+++ src/hex2modhex.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+HEX_SYM=0123456789abcdefABCDEF
+MOD_SYM=cbdefghijklnrtuvlnrtuv
+
+tr $HEX_SYM $MOD_SYM
---
+++ src/modhex2hex.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+MOD_SYM=cbdefghijklnrtuvCBDEFGHIJKLNRTUV
+HEX_SYM=0123456789abcdef0123456789ABCDEF
+
+tr $MOD_SYM $HEX_SYM

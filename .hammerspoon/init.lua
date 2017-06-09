1dff0c051172aff956747a2965496ed3befa870c
Joe Cridge <joe.cridge@me.com>
2017-06-09 21:19:27 +0100

    Set Hyper as launcher terminal

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,6 @@
 -- Jeo keyboard layout.
 jeo = require 'jeospoon'
 jeo.setEditor('MacVim')
+jeo.setTerminal('Hyper')
 jeoEventTap = hs.eventtap.new(jeo.KEY_EVENTS, jeo.handleKeyEvent)
 jeoEventTap:start()

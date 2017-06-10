4a7cb01bdbdc276880031dac3b24c4815ddab67c
Joe Cridge <joe.cridge@me.com>
2017-06-10 01:14:37 +0100

    Enable forward delete files in Finder

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,38 @@
--- Jeo keyboard layout.
+-------------------------------------------------------------------------------
+-- Jeo keyboard layout
+-------------------------------------------------------------------------------
+
 jeo = require 'jeospoon'
+
 jeo.setEditor('MacVim')
 jeo.setTerminal('Hyper')
-jeoEventTap = hs.eventtap.new(jeo.KEY_EVENTS, jeo.handleKeyEvent)
+
+jeoEventTap = hs.eventtap.new(jeo.KEY_EVENTS, jeo.handleKeyEvent):start()
 jeoEventTap:start()
+
+
+-------------------------------------------------------------------------------
+-- Allow forward delete to remove files in Finder
+-------------------------------------------------------------------------------
+
+finderDeleteTap = hs.eventtap.new(jeo.KEY_EVENTS, function(event)
+  if hs.keycodes.map[event:getKeyCode()] == 'forwarddelete' then
+    local keyDown = event:getType() == hs.eventtap.event.types.keyDown
+    local newEvent = hs.eventtap.event.newKeyEvent({ 'cmd' }, 'delete', keyDown)
+    newEvent:setProperty(42, 5462350) -- See jeospoon/init.lua
+    return true, { newEvent }
+  end
+end)
+
+function toggleFinderDelete(appName, eventType)
+  if appName == 'Finder' then
+    if eventType == hs.application.watcher.activated then
+      finderDeleteTap:start()
+    elseif eventType == hs.application.watcher.deactivated then
+      finderDeleteTap:stop()
+    end
+  end
+end
+
+finderDeleteWatcher = hs.application.watcher.new(toggleFinderDelete)
+finderDeleteWatcher:start()

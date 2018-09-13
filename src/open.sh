282ab64bd93db4c963e56c95486a9f87b8992e58
Joe Cridge <joecridge@gambitresearch.com>
2018-09-13 20:34:01 +0100

    Add --no-desktop flag to nautilus

--- src/open.sh
+++ src/open.sh
@@ -4,5 +4,5 @@
 # Usage: $ open /path/to/dir
 # Note: this overrides /bin/open
 
-nautilus $1 &> /dev/null &  # Ignore icon not found error
+nautilus --no-desktop $1 &> /dev/null &  # Ignore icon not found error
 exit 0

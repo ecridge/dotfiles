19d959192ab2eb9208011ccc66705b5063092467
Joe Cridge <joe.cridge@me.com>
2019-01-28 15:16:42 +0000

    Check in iPython config

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+
+c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalInteractiveShell.editing_mode = "vi"
+
+c.TerminalInteractiveShell.highlighting_style = "fruity"

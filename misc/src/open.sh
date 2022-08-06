55736f70b1680cc92b4f75ecac372227674eddc4
Joe Cridge <joe.cridge@me.com>
2022-08-06 02:26:32 +0100

    Consolidate gnome config

--- misc/src/open.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Open a directory in Nautilus file browser
-# Usage: $ open /path/to/dir
-# Note: this overrides /bin/open
-
-nautilus --no-desktop $1 &> /dev/null &  # Ignore icon not found error
-exit 0

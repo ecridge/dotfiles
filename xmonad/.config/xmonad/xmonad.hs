6d0f0626efd414142497b760e1c2cd0d70cb7dd3
Eden Cridge <eden.cridge@gmail.com>
2023-08-29 18:40:42 +0100

    feat: run xmobar

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,12 +1,19 @@
 import XMonad
 
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
 
+
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ def
+main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
+
+myConfig = def
     { modMask = mod4Mask  -- Rebind Mod to the Super key
     , terminal = "alacritty"
     }

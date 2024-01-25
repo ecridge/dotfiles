adb3e2f5d59a85a7ed18fdc90c82a24ba1fe891d
Eden Cridge <eden.cridge@gmail.com>
2024-01-25 20:55:28 +0000

    feat(xmonad): configure picom

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -5,6 +5,8 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
+import XMonad.Layout.Spacing
+
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
@@ -20,8 +22,23 @@ myConfig = def
     , focusedBorderColor = "#a7c080"
     , focusFollowsMouse = False
     , clickJustFocuses = False
+    , layoutHook = smartSpacingWithEdge 4 $ myLayout
     }
     `additionalKeysP`
     [ ("M-s", unGrab *> spawn "scrot -s -F ~/Downloads/'Screen Shot %Y-%m-%d at %H.%M.%S.png'")
     , ("M-x", spawn "xset s activate")  -- Start screen saver (which is configured to lock screen)
     ]
+
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
